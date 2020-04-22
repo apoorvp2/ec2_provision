db418f7da4d545004ddca4c2f7e00f32c2c49a6f
kapilkhanduja25 <kapilkhanduja25@gmail.com>
2020-04-22 20:27:05 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+
+resource "aws_instance" "web" {
+  count = "${var.instance_count}"
+  ami = "${element(var.ami,count.index)}"
+  availability_zone = "${element(var.azs,count.index)}"
+  instance_type = "${var.ins_type}"
+  
+  tags = {
+    Name = "ts${count.index}"
+  }
+
+
+  
---
+++ vars.tf
@@ -0,0 +1,22 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+
+variable "instance_count"{
+  default = "1"
+}
+
+variable "ami"{
+  type = "list"
+  default = ["ami-0a74bfeb190bd404f"]
+}
+
+variable "azs"{
+  type = "list"
+  default = ["ap-south-1a"]
+}
+
+variable "ins_type"{
+  default = "t2.micro"
+}
